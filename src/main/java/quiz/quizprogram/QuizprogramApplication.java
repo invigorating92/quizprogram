838d95d387b3687385e9cb7515b3fcfd66a324b0
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-02-17 11:36:13 +0900

    Initial commit

---
+++ src/main/java/quiz/quizprogram/QuizprogramApplication.java
@@ -0,0 +1,13 @@
+package quiz.quizprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizprogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quiz/quizprogram/QuizprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package quiz.quizprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
